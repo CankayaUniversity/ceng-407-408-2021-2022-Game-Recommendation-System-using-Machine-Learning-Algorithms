ffebe983ae90245b763b743330808d15ecb99e08
Kutay Kabadaş <kutaykbds52@gmail.com>
2022-03-14 21:45:40 +0300

    Login and Register page presenter template implemented.

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Platform, StatusBar, TextInput } from 'react-native';
+import { Register } from './src/Utils/features/RegisterPresenter/Register';
+import { Login } from './src/Utils/features/LoginPresenter/LoginPresenter';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        {/* <Register /> */}
+        <Login></Login>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "#00bcd4",
   },
 });
