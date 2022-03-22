fdfdb0a410a254167f41587d52c09c9987b0c14d
Berfin Varlı <berfinvarli06@gmail.com>
2022-03-22 14:22:41 +0300

    updated

--- PythonSrc/main.py
+++ PythonSrc/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sklearn as sk
 import numpy as np
+from sklearn.model_selection import train_test_split
 # import matplotlib.pyplot as plt
 
 from lightfm.datasets import fetch_movielens
@@ -26,11 +27,18 @@ user_dict = create_user_dict(interactions=interactions)
 movies_dict = create_item_dict(df = ratings,
                                id_col = 'appid',
                                name_col = 'name')
+X = ratings.iloc[:, :-1]
+y = ratings.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+print(X_train)
+print(y_train)
+
 mf_model = runMF(interactions = interactions,
                  n_components = 30,
                  loss = 'warp',
                  epoch = 30,
                  n_jobs = 4)
+print(mf_model)
 rec_list = sample_recommendation_user(model = mf_model,
                                       interactions = interactions,
                                       user_id = 14153959,
